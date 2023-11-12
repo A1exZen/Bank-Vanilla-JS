f00425c2550bb30f8888e9bf6cec3cdd0eb1e960
Alex <AlexComp2021@gmail.com>
2023-11-12 19:30:20 +0300

    implement module styles

---
+++ src/components/layout/layout.component.js
@@ -0,0 +1,16 @@
+export class Layout {
+	constructor({ router, children }) {
+		this.router = router;
+		this.children = children;
+	}
+	render() {
+		const headerHtml = `<header>
+			Header
+			<nav>
+			<a href='/'>Home</a>
+			<a href='/auth'>Auth</a>
+			</nav>
+		</header>`;
+		return `${headerHtml}<main>${this.children}</main>`;
+	}
+}
